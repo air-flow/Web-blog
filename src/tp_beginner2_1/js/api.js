50c88f0cb51b51ec139f7198ff2a049f5f4023ea
updraft <updraft.1970@gmail.com>
2021-05-26 08:44:28 +0900

    feat:api create

---
+++ src/tp_beginner2_1/js/api.js
@@ -0,0 +1,26 @@
+$(function () {
+  $("#new").on("click", function () {
+    result = api("new");
+    $("#content").append(result["item"]);
+  });
+});
+
+function api(type) {
+  result = "";
+  $.ajax({
+    type: "GET",
+    url: "",
+    data: JSON.stringify({ eventType: type }),
+    dataType: "json",
+  })
+    .done(function (data) {
+      // 通信成功時の処理を記述
+      console.log(data);
+      result = data;
+    })
+    .fail(function (data) {
+      // 通信失敗時の処理を記述
+      console.log(data);
+    });
+  return result;
+}
