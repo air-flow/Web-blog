e25df635e9d657204b187e2cf7eae8cff053f75e
updraft <updraft.1970@gmail.com>
2021-05-26 16:54:21 +0900

    feat:all

---
+++ src/tp_beginner2_1/js/all.js
@@ -0,0 +1,24 @@
+$(function () {
+  result = JSON.parse(api("all"));
+  $("#newinfo_hdr").append(result["result"]);
+});
+
+function api(type) {
+  result = "";
+  $.ajax({
+    type: "GET",
+    url: "",
+    data: { eventType: type },
+    dataType: "json",
+  })
+    .done(function (data) {
+      // 通信成功時の処理を記述
+      console.log(data);
+      result = data;
+    })
+    .fail(function (data) {
+      // 通信失敗時の処理を記述
+      console.log(data);
+    });
+  return result;
+}
