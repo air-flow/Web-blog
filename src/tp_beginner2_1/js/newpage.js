c179b82be7465e2aa0851e63b6be41f437c73890
updraft <updraft.1970@gmail.com>
2021-06-01 15:42:39 +0900

    feat:newpage function

--- src/tp_beginner2_1/js/newpage.js
+++ src/tp_beginner2_1/js/newpage.js
@@ -1,18 +1,34 @@
 $(function () {
+  id = localStorage.getItem("newdata");
+  if (typeof id === "undefined") {
+    
+  }
   $("#submit").on("click", function () {
-    id = "";
-    title = $("#page_title").val();
-    contents = $("#contents").val();
-    api("newPage", title, contents);
+    if (typeof id === "undefined") {
+      d = {
+        eventType: type,
+        title: $("#page_title").val(),
+        sentense: $("#contents").val(),
+      };
+      api(d);
+    } else {
+      d = {
+        eventType: type,
+        title: $("#page_title").val(),
+        sentense: $("#contents").val(),
+        blogId: id,
+      };
+      api(d);
+    }
   });
 });
 
-function insert_api(type, t, c) {
+function insert_api(d) {
   result = "";
   $.ajax({
     type: "POST",
     url: "",
-    data: { eventType: type  },
+    data: JSON.stringify(d),
     dataType: "json",
   })
     .done(function (data) {
